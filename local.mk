829011cfe7cdde2c518c5342cf051f8ebc3cb558
Marcio Montenegro <mtuxpe@gmail.com>
2020-09-18 17:44:26 -0300

    Add buildroot_override for host-m4 and host-bison
    Signed-off-by: Marcio Montenegro <mtuxpe@gmail.com>

---
+++ local.mk
@@ -0,0 +1,4 @@
+BISON_OVERRIDE_SRCDIR = buildroot_override/host-bison-3.0.4
+M4_OVERRIDE_SRCDIR = buildroot_override/host-m4-1.4.18
+#GDB_OVERRIDE_SRCDIR = buildroot_override/host-gdb-4ecb98fbc2f94dbe01b69384afbc515107de73df
+
